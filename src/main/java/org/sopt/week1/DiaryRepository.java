a4743e5a6262400fc8997f01c0bda0ebe59b346e
choyeongju <jjudy1301@naver.com>
2024-10-25 15:06:01 +0900

    :recycle: [fix] 충돌 해결 과정에서 오류난 부분 수정

--- src/main/java/org/sopt/week1/DiaryRepository.java
+++ src/main/java/org/sopt/week1/DiaryRepository.java
@@ -14,12 +14,9 @@ public class DiaryRepository {
     private final Map<Long, String> deletedStorage = new ConcurrentHashMap<>();
     private final Map<Long, Integer> patchCount = new ConcurrentHashMap<>(); //수정 횟수
     private final Map<Long, LocalDate> patchDate = new ConcurrentHashMap<>(); // 마지막으로 수정한 날짜
-
-public class DiaryRepository {
-    private final Map<Long, String> storage = new ConcurrentHashMap<>();
     private final AtomicLong numbering = new AtomicLong();
 
-    void save(final Diary diary){
+    void save(final Diary diary) {
 
         // 채번 과정(1을 더한 값을 반환)
         final long id = numbering.addAndGet(1);
@@ -39,7 +36,7 @@ public class DiaryRepository {
             final String body = storage.get(index);
 
             // (2-1) 불러온 값을 구성한 자료구조로 이관
-            if(body != null) {
+            if (body != null) {
                 diaryList.add(new Diary(index, body));
             }
         }
@@ -50,7 +47,7 @@ public class DiaryRepository {
 
     void delete(final Long id) {
         String removedDiary = storage.remove(id);
-        if(removedDiary != null){
+        if (removedDiary != null) {
             deletedStorage.put(id, removedDiary);
             patchCount.remove(id);  // 삭제 시 수정 횟수 제거
             patchDate.remove(id);  // 삭제 시 날짜 제거
@@ -61,14 +58,14 @@ public class DiaryRepository {
         LocalDate today = LocalDate.now();
         LocalDate last = patchDate.getOrDefault(id, today);
 
-        if(!today.equals(last)){
+        if (!today.equals(last)) {
             patchCount.put(id, 0);
             patchDate.put(id, today);
         }
 
         int count = patchCount.getOrDefault(id, 0);
 
-        if(count >= 2){
+        if (count >= 2) {
             throw new LimitEditException();
         }
         storage.replace(id, body);
@@ -86,21 +83,11 @@ public class DiaryRepository {
         storage.remove(id);
     }
 
-    void patch(final Long id, final String body) {
-        /*
-        replace() : key 가 존재할 때에만 값을 변경
-        put() : key 가 존재하지 않으면 새로운 key-value 쌍을 추가
-         */
-        storage.replace(id, body);
-
-    }
-
-    boolean existById(final Long id){
+    boolean existById(final Long id) {
         return storage.containsKey(id);
     }
 
-
-    boolean existByDeletedId(final Long id){
+    boolean existByDeletedId(final Long id) {
         return deletedStorage.containsKey(id);
     }
 }
